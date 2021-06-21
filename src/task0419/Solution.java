bf19457609183287eb7df34d324c661e36c82c91
DodoEkambi <gkhngul2@gmail.com>
2021-06-21 23:39:22 +0300

    task0419 çalışmaya devam

---
+++ src/task0419/Solution.java
@@ -0,0 +1,35 @@
+package task0419;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sNumber1 = reader.readLine();
+        String sNumber2 = reader.readLine();
+        String sNumber3 = reader.readLine();
+        String sNumber4 = reader.readLine();
+        int number1 = Integer.parseInt(sNumber1);
+        int number2 = Integer.parseInt(sNumber2);
+        int number3 = Integer.parseInt(sNumber3);
+        int number4 = Integer.parseInt(sNumber4);
+        int max = number1 ;
+        if(max<number2)
+        {
+            max = number2;
+        }
+        else if (max<number3)
+        {
+            max = number3;
+        }
+        else if(max<number4)
+        {
+            max= number4;
+        }
+        else
+        {
+            System.out.println(max);
+        }
+    }
+}
