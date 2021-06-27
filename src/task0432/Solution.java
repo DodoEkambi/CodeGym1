0726c741fb0c14e000fe6902a4542eda890d4d28
DodoEkambi <gkhngul2@gmail.com>
2021-06-27 15:08:26 +0300

    task0432//Ekrandan String alma ve ekrana kaç kere basılacağını alıp ekrana bastırma

---
+++ src/task0432/Solution.java
@@ -0,0 +1,17 @@
+package task0432;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        int n = Integer.parseInt(bufferedReader.readLine());
+        while(n>0)
+        {
+            System.out.println(s);
+            n--;
+        }
+    }
+}
