b7c74a9797c06c15f4e8dd20aa1fa336965a46b4
DodoEkambi <gkhngul2@gmail.com>
2021-06-27 15:02:48 +0300

    task0431

---
+++ src/task0431/Solution.java
@@ -0,0 +1,12 @@
+package task0431;
+
+public class Solution {
+    public static void main(String[] args) {
+        int count = 10;
+        while(count>=1)
+        {
+            System.out.println(count);
+            count--;
+        }
+    }
+}
