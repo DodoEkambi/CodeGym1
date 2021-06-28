e2de333696e1ec991b00b6c67b4ff7fe5cbd7db5
DodoEkambi <gkhngul2@gmail.com>
2021-06-28 19:37:27 +0300

    task0436//kullanıcıdan dikdörtgen uzunluğunu alıp o kadar sayıda ekrana 8 basıyor.

---
+++ src/task0436/Solution.java
@@ -0,0 +1,20 @@
+package task0436;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
+        for(int i=m;i>0;i--)
+        {
+            for(int j=n;j>0;j--)
+            {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}
