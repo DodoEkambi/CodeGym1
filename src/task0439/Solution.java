4ef82be4d4af8254688f9c64c050c2346fcf331e
DodoEkambi <gkhngul2@gmail.com>
2021-07-01 09:02:34 +0300

    task0439//Kullanıcıdan isim alıp ekrana 100 sefer bastıran metod.

---
+++ src/task0439/Solution.java
@@ -0,0 +1,15 @@
+package task0439;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String name = bufferedReader.readLine();
+        for(int i = 1 ; i<=10;i++)
+        {
+            System.out.println(name + " loves me.");
+        }
+    }
+}
