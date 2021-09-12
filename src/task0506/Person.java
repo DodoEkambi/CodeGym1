3079ce58b7ee0e1e857b413eaca9adb5945e1161
DodoEkambi <gkhngul2@gmail.com>
2021-09-12 14:34:28 +0300

    task0506//İnsan nesnesinin adı, yaşı, cinsiyeti, adres değişkenlerinin tanımlanması.

---
+++ src/task0506/Person.java
@@ -0,0 +1,9 @@
+package task0506;
+
+public class Person {
+    public String name;
+    public int age;
+    public String address;
+    public char sex;
+
+}
---
+++ src/task0506/Solution.java
@@ -0,0 +1,7 @@
+package task0506;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
