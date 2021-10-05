b26ee3731bf391acf1244b592a42df9203b0f5d3
DodoEkambi <gkhngul2@gmail.com>
2021-10-05 22:21:01 +0300

    task0518//Dog sınıfımızı ve yapılandırıcılarını oluşturduk.

---
+++ src/task05/task0518/Dog.java
@@ -0,0 +1,24 @@
+package task05.task0518;
+
+public class Dog {
+    private String name = "Sabahattin";
+    private int height=1;
+    private String color = "black";
+
+    public Dog(String name){
+        this.name = name;
+    }
+    public Dog(String name , int height){
+        this.name = name;
+        this.height = height;
+    }
+    public Dog(String name, int height, String color){
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
