3bde2a9ccc92b5068b94b8d8c26439b8c6ff1dca
DodoEkambi <gkhngul2@gmail.com>
2021-10-05 22:26:26 +0300

    task0519//Circle sınıfımızı ve yapılandırıcıları oluşturduk.

---
+++ src/task05/task0519/Circle.java
@@ -0,0 +1,33 @@
+package task05.task0519;
+
+public class Circle {
+    private int centerX=0;
+    private int centerY=0;
+    private int radius=0;
+    private int width=0;
+    private int color=0;
+
+    public Circle(int centerX, int centerY , int radius){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+    public Circle(int centerX, int centerY , int radius, int width){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+    }
+    public Circle(int centerX, int centerY , int radius,int width, int color){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
