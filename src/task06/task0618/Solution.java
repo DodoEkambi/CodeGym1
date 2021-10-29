0ed0020b8722e37dc8c50ee00ac002f53bc6aa7f
DodoEkambi <gkhngul2@gmail.com>
2021-10-29 22:15:48 +0300

    task0618//robot yaratıp ekranan bastırır.

---
+++ src/task06/task0618/Solution.java
@@ -0,0 +1,12 @@
+package task06.task0618;
+
+public class Solution {
+    public static class KissMyShinyMetalRearActuator{
+
+    }
+
+    public static void main(String[] args) {
+        KissMyShinyMetalRearActuator robot = new KissMyShinyMetalRearActuator();
+        System.out.println(robot);
+    }
+}
