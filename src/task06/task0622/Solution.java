72fffef703410e7b9c810b0eed57eda32d473db9
DodoEkambi <gkhngul2@gmail.com>
2021-10-31 10:18:54 +0300

    Task eklendi

--- src/task06/task0622/Solution.java
+++ src/task06/task0622/Solution.java
@@ -4,42 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /*
-Task: Each cat has a name and a mother.
-Create a class that describes this situation.
-Create two objects: a daughter cat and a mother.
-Display them on the screen.
 
-New task: Each cat has a name, a father, and a mother.
-Edit Cat so that it reflects these relationships.
-Create 6 objects: grandfather (the father's father), grandmother (the mother's mother), father, mother, son, daughter.
-Display them all in the following order: grandfather, grandmother, father, mother, son, daughter.
-
-Example input:
-Grandfather Tiger
-Grandmother Puss
-Father Oscar
-Mother Missy
-Son Simba
-Daughter Coco
-
-Example output:
-The cat's name is Grandfather Tiger, no mother, no father
-The cat's name is Grandmother Puss, no mother, no father
-The cat's name is Father Oscar, no mother, Grandfather Tiger is the father
-The cat's name is Mother Missy, Grandmother Puss is the mother, no father
-The cat's name is Son Simba, Mother Missy is the mother, Father Oscar is the father
-The cat's name is Daughter Coco, Mother Missy is the mother, Father Oscar is the father
-
-Requirements:
-•	The program should read in the names of 6 cats in the specified order.
-•	The main method should create 6 Cat objects.
-•	The program should display 6 lines with information about the cats.
-•	The line about the grandfather (first line) must match the conditions.
-•	The line about the grandmother (second line) must match the conditions.
-•	The line about the father (third line) must match the conditions.
-•	The line about the mother (fourth line) must match the conditions.
-•	The line about the son (fifth line) must match the conditions.
-•	The line about the daughter (sixth line) must match the conditions.
  */
 
 public class Solution {
